877deb1ed6a76f0aee9c3e2532306bcb3d8b587d
AkimovaOlesya <lesya.akimova93@gmail.com>
2022-01-17 18:02:28 +0700

    Make base UITableView Make UITableView
    Создана таблица с простым наполнением

--- SettingApp/ViewController.swift
+++ SettingApp/ViewController.swift
@@ -7,13 +7,51 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+struct Settings {
+    let text: String
+    let image: UIImage?
+    let imageBackgroundColor: UIColor
+    let handler: (() -> Void)
+}
+
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+
+    private let tableView: UITableView = {
+        let tableView = UITableView(frame: .zero, style: .grouped)
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        return tableView
+    }()
+
+    private lazy var cells = [Settings]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
+        configure()
+            title = "Settings"
+            view.addSubview(tableView)
+            tableView.delegate = self
+            tableView.dataSource = self
+            tableView.frame = view.bounds
+        }
 
+        private func configure() {
+            self.cells = Array(0...100).compactMap({
+                Settings(text: "Item \($0)", image: UIImage(systemName: "house"), imageBackgroundColor: .systemPink) {
+
+                }
+            })
+        }
+
+        func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+            return cells.count
+        }
+
+        func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+            let model = cells[indexPath.row]
+            let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+            cell.textLabel?.text = model.text
+            return cell
+        }
 
 }
 
