139b2e69955fdfe2918eb606276c4f2e598aeff6
AkimovaOlesya <lesya.akimova93@gmail.com>
2022-01-17 23:05:20 +0700

    Made separators and custom cells
    Добавила разделители для групп настроек, сделала несколько кастомных ячеек,  для примера.

--- SettingApp/SettingTableViewCell.swift
+++ SettingApp/SettingTableViewCell.swift
@@ -14,7 +14,7 @@ class SettingTableViewCell: UITableViewCell {
     private let iconContainer: UIView = {
         let iconContainer = UIView()
         iconContainer.clipsToBounds = true
-        iconContainer.layer.cornerRadius = 10
+        iconContainer.layer.cornerRadius = 8
         iconContainer.layer.masksToBounds = true
 
         return iconContainer
@@ -53,15 +53,15 @@ class SettingTableViewCell: UITableViewCell {
     override func layoutSubviews() {
         super.layoutSubviews()
         let size: CGFloat = contentView.frame.size.height - 12
-        iconContainer.frame = CGRect(x: 10, y: 6, width: size, height: size)
+        iconContainer.frame = CGRect(x: 15, y: 6, width: size, height: size)
 
         let imageSize: CGFloat = size/1.5
-        iconImageView.frame = CGRect(x: 0, y: 0, width: imageSize, height: imageSize)
-        iconImageView.center = iconContainer.center
+        iconImageView.frame = CGRect(x: (size - imageSize)/2, y: (size - imageSize)/2, width: imageSize, height: imageSize)
+     //   iconImageView.center = iconContainer.center
 
-        label.frame = CGRect(x: 15 + iconContainer.frame.size.width,
+        label.frame = CGRect(x: 25 + iconContainer.frame.size.width,
                              y: 0,
-                             width: contentView.frame.size.width - 15 - iconContainer.frame.size.width,
+                             width: contentView.frame.size.width - 20 - iconContainer.frame.size.width,
                              height: contentView.frame.size.height)
     }
 
--- SettingApp/ViewController.swift
+++ SettingApp/ViewController.swift
@@ -7,6 +7,11 @@
 
 import UIKit
 
+struct Section {
+    let title: String
+    let options: [Settings]
+}
+
 struct Settings {
     let text: String
     let image: UIImage?
@@ -22,7 +27,7 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         return tableView
     }()
 
-    private lazy var cells = [Settings]()
+    private lazy var models = [Section]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -35,19 +40,57 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         }
 
         private func configure() {
-            self.cells = Array(0...100).compactMap({
-                Settings(text: "Item \($0)", image: UIImage(systemName: "house"), imageBackgroundColor: .systemPink) {
+            models.append(Section(title: "Связь", options: [
+                
+                Settings(text: "Авиарежим", image: UIImage(systemName: "airplane"), imageBackgroundColor: .systemOrange) {
+                    print("Tapped first cell")
+                },
+                
+                Settings(text: "Wi-Fi", image: UIImage(systemName: "wifi"), imageBackgroundColor: .systemPink) {
+                },
+                
+                Settings(text: "Bluetooth", image: UIImage(systemName: "Bluetooth"), imageBackgroundColor: .link) {
+                },
+                
+                Settings(text: "Сотовая связь", image: UIImage(systemName: "mobile data"), imageBackgroundColor: .systemGreen) {
+                },
+                
+                Settings(text: "Режим модема", image: UIImage(systemName: "personal hotspot"), imageBackgroundColor: .systemGreen) {
+                },
+                
+            ]))
+            models.append(Section(title: "Основные", options: [
+                
+                Settings(text: "Основные", image: UIImage(systemName: "general"), imageBackgroundColor: .systemPink) {
+                },
+                
+                Settings(text: "Пункт управления", image: UIImage(systemName: "control station"), imageBackgroundColor: .systemOrange) {
+                },
+                
+                Settings(text: "Экран и яркость", image: UIImage(systemName: "screen and brightness"), imageBackgroundColor: .link) {
+                },
+                
+                Settings(text: "Экран домой", image: UIImage(systemName: "screen house"), imageBackgroundColor: .systemGreen) {
 
-                }
-            })
+                },
+                
+            ]))
         }
+    
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        let section = models[section]
+        return section.title
+    }
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return models.count
+    }
 
         func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-            return cells.count
+            return models[section].options.count
         }
 
         func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-            let model = cells[indexPath.row]
+            let model = models[indexPath.section].options[indexPath.row]
             guard let cell = tableView.dequeueReusableCell(
                 withIdentifier: SettingTableViewCell.identifier,
                 for: indexPath) as? SettingTableViewCell else {
@@ -56,6 +99,12 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
             cell.configure(with: model)
             return cell
         }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+        let model = models[indexPath.section].options[indexPath.row]
+        model.handler()
+    }
 
 }
 
