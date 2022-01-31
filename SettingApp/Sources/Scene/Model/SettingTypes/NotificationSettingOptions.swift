3b1a60172dd1a47f0521ebd71bf586dc4b492ca2
AkimovaOlesya <lesya.akimova93@gmail.com>
2022-01-31 15:41:24 +0700

    Add Models

---
+++ SettingApp/Sources/Scene/Model/SettingTypes/NotificationSettingOptions.swift
@@ -0,0 +1,15 @@
+//
+//  NotificationSettingOptions.swift
+//  SettingApp
+//
+//  Created by Леся Акимова on 31.01.2022.
+//
+
+import UIKit
+
+struct NotificationSettingOptions {
+  let title: String
+  let icon: UIImage?
+  let iconBackgroundColor: UIColor
+  let notification: String
+}
